33eedbe7908e28293938c8c2eb7efdab4a69455d
ubeuu <ubeu25@gmail.com>
2024-08-19 19:08:03 +0900

    first commit

---
+++ src/main/java/com/example/web_scrap/WebScrapApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_scrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebScrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebScrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/web_scrap/WebScrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_scrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
