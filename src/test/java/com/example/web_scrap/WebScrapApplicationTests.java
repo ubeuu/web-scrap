59493fb477eb296494164816275aff4abe33219e
ubeuu <ubeu25@gmail.com>
2024-08-27 00:49:21 +0900

    통합 테스트 코드 주석 처리

--- src/test/java/com/example/web_scrap/WebScrapApplicationTests.java
+++ src/test/java/com/example/web_scrap/WebScrapApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.web_scrap;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class WebScrapApplicationTests {
 
 	@Test
