522b8e69000beaf6be44b0077c95e1bdded30623
ubeuu <ubeu25@gmail.com>
2024-09-04 16:05:07 +0900

    구글 버전 로그 출력

--- src/main/java/com/example/web_scrap/config/WebCrawler.java
+++ src/main/java/com/example/web_scrap/config/WebCrawler.java
@@ -1,5 +1,6 @@
 package com.example.web_scrap.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -7,6 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+@Slf4j
 @Configuration
 public class WebCrawler {
 
@@ -14,7 +19,7 @@ public class WebCrawler {
     private String driverPath;
 
     @Bean
-    public WebDriver webDriver() {
+    public WebDriver webDriver() throws IOException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless"); //헤드리스 모드로 실행
         chromeOptions.addArguments("window-size=1920x1080");
@@ -22,6 +27,16 @@ public class WebCrawler {
         chromeOptions.addArguments("--no-sandbox"); //샌드박스 모드 비활성화
         chromeOptions.addArguments("--disable-dev-shm-usage"); // /dev/shm 사용 비활성화
         chromeOptions.addArguments("--disable-gpu"); //GPU 가속 비활성화
-        return new ChromeDriver(chromeOptions);
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        String[] command = {
+                "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+                "--version"
+        };
+        Process process = new ProcessBuilder(command).start();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String version = reader.readLine();
+        log.info("-- Chrome Version: " + version);
+
+        return driver;
     }
 }
