b0061ecdabc6615d1367004b0743a75c76e86693
ubeuu <ubeu25@gmail.com>
2024-10-23 16:18:48 +0900

    hotfix: healthcheck

--- src/main/java/com/example/web_scrap/WebController.java
+++ src/main/java/com/example/web_scrap/WebController.java
@@ -1,7 +1,9 @@
 package com.example.web_scrap;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class WebController {
     @GetMapping("/healthcheck")
     public String healthCheck(){
